1d483189262507813e53508c4583a987ae5646de
Alistair Adcroft <aadcroft@princeton.edu>
1998-06-09 15:58:36 +0000

    Various corrections:  o do_gterm_blocking..() is fixed to work with new time-stepping  o CPP_OPTIONS.h now contains #define ALLOW_CD

--- model/inc/CPP_OPTIONS.h
+++ model/inc/CPP_OPTIONS.h
@@ -1,2 +1,6 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/CPP_OPTIONS.h,v 1.2 1998/04/24 02:11:36 cnh Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/CPP_OPTIONS.h,v 1.3 1998/06/09 15:58:35 adcroft Exp $
 C
+
+#undef ALLOW_CD
+
+#include "CPP_EEOPTIONS.h"
