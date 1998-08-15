f4d804f7aa5c0f8e07fa3b4a2d4620e8996e85e0
Chris Hill <cnh@mit.edu>
1998-08-15 16:58:55 +0000

    Changes for moving from z -> r as a vertical coordinate Required changing r -> recip for reciprocal variable notation.

--- model/inc/RA_MACROS.h
+++ model/inc/RA_MACROS.h
@@ -1,25 +1,25 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/Attic/ZA_MACROS.h,v 1.1 1998/05/30 02:10:16 cnh Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/RA_MACROS.h,v 1.1 1998/08/15 16:55:48 cnh Exp $
 C
 C     /==========================================================\
-C     | ZA_MACROS.h                                              |
+C     | RA_MACROS.h                                              |
 C     |==========================================================|
 C     | These macros are used to reduce memory requirement and/or|
 C     | memory references when variables are fixed along a given |
 C     | axis or axes.                                            |
 C     \==========================================================/
 
-#ifdef ZA_CONST
-#define  _zA(i,j,bi,bj) zA(1,1,1,1)
+#ifdef RA_CONST
+#define  _rA(i,j,bi,bj) rA(1,1,1,1)
 #endif
 
-#ifdef ZA_FX
-#define  _zA(i,j,bi,bj) zA(i,1,bi,1)
+#ifdef RA_FX
+#define  _rA(i,j,bi,bj) rA(i,1,bi,1)
 #endif
 
-#ifdef ZA_FY
-#define  _zA(i,j,bi,bj) zA(1,j,1,bj)
+#ifdef RA_FY
+#define  _rA(i,j,bi,bj) rA(1,j,1,bj)
 #endif
 
-#ifndef _zA
-#define  _zA(i,j,bi,bj) zA(i,j,bi,bj)
+#ifndef _rA
+#define  _rA(i,j,bi,bj) rA(i,j,bi,bj)
 #endif
