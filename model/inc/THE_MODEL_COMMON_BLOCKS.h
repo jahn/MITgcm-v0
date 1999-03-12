225073c88bed0031ce6c45bb8089a22f0fcf0464
Alistair Adcroft <aadcroft@princeton.edu>
1999-03-12 16:44:04 +0000

    Some simple fixes that save a bit of memory.  o AVER.h was included even if not be used.  o gW, gWnm1 and cg3d_nm1 existed in DYNVARS.h and are only    needed in the non-hydrostatic version (which this isn't).

--- model/inc/THE_MODEL_COMMON_BLOCKS.h
+++ model/inc/THE_MODEL_COMMON_BLOCKS.h
@@ -1,8 +1,10 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/THE_MODEL_COMMON_BLOCKS.h,v 1.3 1998/07/03 18:42:11 adcroft Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/THE_MODEL_COMMON_BLOCKS.h,v 1.4 1999/03/12 16:44:04 adcroft Exp $
 C
 #include "PARAMS.h"
 #include "GRID.h"
 #include "DYNVARS.h"
 #include "FFIELDS.h"
 #include "CG2D.h"
+#ifdef INCLUDE_DIAGNOSTICS_INTERFACE_CODE
 #include "AVER.h"
+#endif
