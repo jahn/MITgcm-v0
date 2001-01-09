3f2cccb6c78f5404eda673b8db4e60a482b3b13d
Alistair Adcroft <aadcroft@princeton.edu>
2001-01-09 21:26:14 +0000

    Tentative implimentatin of OBCs.

--- model/inc/GW.h
+++ model/inc/GW.h
@@ -1,4 +1,4 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/Attic/GW.h,v 1.1 1999/03/22 15:54:03 adcroft Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/Attic/GW.h,v 1.1.2.1 2001/01/09 21:26:05 adcroft Exp $
 
 #ifdef ALLOW_NONHYDROSTATIC
 C     /==========================================================\
@@ -10,8 +10,7 @@ C     wVel  - zonal velocity (m/s, i=1 held at western face)
 C     gX, gXNM1 - Time tendencies at current and prvious time levels.
 
       COMMON /GW_R/ 
-     &                   wVel, gw,gwNm1
-      _RL  wVel (1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+     &                   gw,gwNm1
       _RL  gW(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
       _RL  gWnm1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
 
