187d5ba8b953dd25fde2c03c3f788498af2d0b8c
Alistair Adcroft <aadcroft@princeton.edu>
2001-02-02 21:36:34 +0000

    Merged changes from branch "branch-atmos-merge" into MAIN (checkpoint34)  - substantial modifications to algorithm sequence (dynamics.F)  - packaged OBCS, Shapiro filter, Zonal filter, Atmospheric Physics

---
+++ pkg/aim/atparam.h
@@ -0,0 +1,9 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/aim/Attic/atparam.h,v 1.2 2001/02/02 21:36:29 adcroft Exp $
+C $Namer: $
+
+C     MITgcm declaration of grid size. 
+C     Latitudinal extent is one less than MITgcm ( i.e. NY-1)
+C     because MITgcm has dummy layer of land at northern most
+C     edge.
+#include "SIZE.h"
+#include "atparam0.h"
---
+++ pkg/zonal_filt/ZONAL_FILT_OPTIONS.h
@@ -0,0 +1,3 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/zonal_filt/ZONAL_FILT_OPTIONS.h,v 1.2 2001/02/02 21:36:30 adcroft Exp $
+
+#include "CPP_OPTIONS.h"
