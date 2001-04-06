70b9ccba5784a4884f8399ff9902e375fa6ca85d
Jean-Michel Campin <jmc@ocean.mit.edu>
2001-04-06 12:50:49 +0000

    add the 2D-solver Tolerence and a flag to switch off the RHS normalisation

--- model/inc/CG2D.h
+++ model/inc/CG2D.h
@@ -1,4 +1,4 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/CG2D.h,v 1.7.2.2 2001/04/02 20:05:04 adcroft Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/model/inc/CG2D.h,v 1.7.2.3 2001/04/06 12:50:49 jmc Exp $
 C $Name:  $
  
 C     /==========================================================\
@@ -7,6 +7,11 @@ C     | o Two-dimensional conjugate gradient solver header.      |
 C     |==========================================================|
 C     | Internal (private) data structures.                      |
 C     \==========================================================/
+
+C--   COMMON /CG2D_I_L/ cg2dNormaliseRHS
+C     cg2dNormaliseRHS - flag set to TRUE if normalise RHS in the Solver
+      COMMON /CG2D_I_L/ cg2dNormaliseRHS
+      LOGICAL cg2dNormaliseRHS
 
 C--   COMMON /CG2D_R/ DEL**2 Laplacian operators
 C     aW2d - East-west operator.
@@ -19,17 +24,19 @@ C     alphaBuf
 C     errBuf
 C     nrmBuf
 C     cg2dNorm - A matrix normalisation factor.
+C     cg2dTolerance - cg2d solver Tolerance (solver unit = m2/s2 or no unit,
+C                                            depending on cg2dNormaliseRHS) 
       COMMON /CG2D_I_R/
      &      aW2d,
      &      aS2d,
      &      pW, pS, pC,
-     &      cg2dNorm
+     &      cg2dNorm, cg2dTolerance
       _RS  aW2d (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RS  aS2d (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RS  pW   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RS  pS   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RS  pC   (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL  cg2dNorm
+      _RL  cg2dNorm, cg2dTolerance
 
 C--   COMMON /CG2D_I_WK_R/  Work array common block
 C     cg2d_q - Intermediate matrix-vector product term
