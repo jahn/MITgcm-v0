d6f9c0c6f7a7203d84492a40a38941c4a815c829
Chris Hill <cnh@mit.edu>
2001-09-21 03:55:50 +0000

    Starting to bring comments up to date and format comments for document extraction of "prototypes".

--- eesupp/inc/MPI_INFO.h
+++ eesupp/inc/MPI_INFO.h
@@ -1,6 +1,12 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/eesupp/inc/MPI_INFO.h,v 1.2 2001/02/04 14:38:42 cnh Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/eesupp/inc/MPI_INFO.h,v 1.3 2001/09/21 03:54:36 cnh Exp $
 C $Name:  $
+CBOP
+C      !ROUTINE: MPI_INFO.h
+C      !INTERFACE:
+C      include "MPI_INFO.h"
+C      !DESCRIPTION:
 C      Parameters used with MPI.
+CEOP
 
        COMMON /MPI_INFO/
      &  mpi_pid,     mpi_np,
