2f51e4b0738cdd4cec04430c9d656c2ac7fdffc9
Patrick Heimbach <heimbach@mit.edu>
2002-03-24 04:06:54 +0000

    Modifying carbon verif. o including GMRedi o including EXACT_CONSRV o test cost functions modified

---
+++ pkg/shap_filt/SHAP_FILT_TRACER.h
@@ -0,0 +1,9 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/shap_filt/Attic/SHAP_FILT_TRACER.h,v 1.2 2001/05/29 14:01:40 adcroft Exp $
+C $Name:  $
+C
+C     /==========================================================\
+C     | SHAP_FILT_TRACER.h                                       |
+C     | o Globals used by Fortran shapiro filter routine.        |
+C     \==========================================================/
+      COMMON / SHFT_COMMON_R8 / tmpFld
+      _RL tmpFld(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
