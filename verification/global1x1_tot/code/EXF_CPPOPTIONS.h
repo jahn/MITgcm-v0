49de4df7585ea547afe9b60e551916d674d3c8ec
Patrick Heimbach <heimbach@mit.edu>
2002-04-04 20:09:28 +0000

    Added global 1x1 deg. configuration to repository

---
+++ verification/global1x1_tot/code/EXF_CPPOPTIONS.h
@@ -0,0 +1,7 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/global1x1_tot/code/Attic/EXF_CPPOPTIONS.h,v 1.1.2.1 2002/04/04 20:09:27 heimbach Exp $
+C
+#include "CPP_OPTIONS.h"
+C 
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+C
