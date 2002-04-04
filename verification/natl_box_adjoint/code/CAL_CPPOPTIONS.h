3ed8f96d88a058f5b0598f394098d17337bb44db
Patrick Heimbach <heimbach@mit.edu>
2002-04-04 11:42:28 +0000

    o cleaning verif. exp. o adding AUTODIFF_MONITOR flag

--- verification/natl_box_adjoint/code/CAL_CPPOPTIONS.h
+++
@@ -1,4 +0,0 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/natl_box_adjoint/code/Attic/CAL_CPPOPTIONS.h,v 1.1.2.1 2002/02/05 20:34:35 heimbach Exp $
-
-#include "CPP_OPTIONS.h"
-
