89c690b4b96eb348880f3908c6fb36231c9c974a
Patrick Heimbach <heimbach@mit.edu>
2002-04-08 20:27:13 +0000

    These changes constitute a patch to release1. They were made on a development branch called "release1_final" and are on the main trunk between chkpt44d_post and checkpoint44h_post along with other changes.
    This code is equivalent to chkpt44d_post with the following patches:
      - AD-related changes for GMRedi
      - fixes i KPP (delZ -> drF)
      - hook to OBCS songe layer code in external_forcing
      - errorMessageUnit non-zero in eeboot.F
      - modified test cost function and carbon verif.

--- verification/carbon/code/CPP_OPTIONS.h
+++ verification/carbon/code/CPP_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/carbon/code/Attic/CPP_OPTIONS.h,v 1.5.4.1 2002/04/08 20:10:40 heimbach Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/carbon/code/Attic/CPP_OPTIONS.h,v 1.5.4.2 2002/04/08 20:27:13 heimbach Exp $
 C
 C CPP flags controlling which code in included in the files that
 C will be compiled.
@@ -132,6 +132,9 @@ C          because the old code did not have no-slip BCs
 
 C o Read/write of checkpoint files for restarting.
 #undef OLD_STYLE_WITH_MANY_FILES
+
+C o Exact volume conservation
+#define EXACT_CONSERV
 
 C o Add passive tracer advection routines
 #define ALLOW_PASSIVE_TRACER
