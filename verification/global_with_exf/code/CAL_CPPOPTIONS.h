eff380dac3ced2c4e01350d01874684d7e3a81f2
Patrick Heimbach <heimbach@mit.edu>
2002-11-07 17:15:45 +0000

    Adding new verification experiment global_with_exf. This verification experiment is almost identical to the global_ocean.90x40x15 experiment, except that it uses the exf (and cal) package instead of the external_fields_load routine (cf README file).

---
+++ verification/global_with_exf/code/CAL_CPPOPTIONS.h
@@ -0,0 +1,4 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/global_with_exf/code/Attic/CAL_CPPOPTIONS.h,v 1.1.2.1 2002/11/07 17:15:41 heimbach Exp $
+
+#include "CPP_OPTIONS.h"
+
---
+++ verification/global_with_exf/code/EXF_CPPOPTIONS.h
@@ -0,0 +1,7 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/global_with_exf/code/Attic/EXF_CPPOPTIONS.h,v 1.1.2.1 2002/11/07 17:15:41 heimbach Exp $
+C
+#include "CPP_OPTIONS.h"
+C 
+C CPP flags controlling which code is included in the files that
+C will be compiled.
+C
