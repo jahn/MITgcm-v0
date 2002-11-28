0051b4f332ad0830ff0d83dbd07d171705a618b8
Patrick Heimbach <heimbach@mit.edu>
2002-11-28 14:20:57 +0000

    Adding setup with explicit differentiaton of pressure solver.

---
+++ verification/natl_box_adjoint/code_cg2d/tamc_keys.h
@@ -0,0 +1,8 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/natl_box_adjoint/code_cg2d/Attic/tamc_keys.h,v 1.1.2.1 2002/11/28 14:20:57 heimbach Exp $
+C
+C     /==========================================================\
+C     | tamc_keys.h keys required by TAMC for record computation |
+C     |==========================================================|
+C     \==========================================================/
+      integer           key, ikey, idkey, icg2dkey
+      common /tamckeys/ key, ikey, idkey, icg2dkey
