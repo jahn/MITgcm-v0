077704f1f0444b95c039a428c02b470f467f5a88
Patrick Heimbach <heimbach@mit.edu>
2002-11-28 14:17:47 +0000

    Updating verif. natl_box_adjoint with GMRedi.

--- verification/natl_box_adjoint/code/GMREDI_OPTIONS.h
+++ verification/natl_box_adjoint/code/GMREDI_OPTIONS.h
@@ -1,3 +1,5 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/natl_box_adjoint/code/Attic/GMREDI_OPTIONS.h,v 1.1.2.2 2002/11/28 14:17:47 heimbach Exp $
+C $Name:  $
 
 C CPP options file for GM/Redi package
 C
@@ -9,6 +11,12 @@ C GM/Redi is enabled with ALLOW_GMREDI in CPP_OPTIONS.h
 
 #ifdef ALLOW_GMREDI
 
+C Specify as tapering scheme either 'orig' or 'clipping',
+C otherwise one of the following is used:
+C 'linear', 'gkw91', 'dm95', 'ldd97'
+#undef GM_TAPER_ORIG_CLIPPING
+#undef GM_TAPER_AC02
+#define GM_TAPER_REST
 
 C This allows the leading diagonal (top two rows) to be non-unity
 C (a feature required when tapering adiabatically).
