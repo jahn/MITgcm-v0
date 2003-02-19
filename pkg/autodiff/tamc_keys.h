79c5120cc49a1aa543a6f1eaa285167af64a0497
Dimitris Menemenlis <Dimitris.Menemenlis@jpl.nasa.gov>
2003-02-19 22:43:52 +0000

    o Removed spurious iikey and idkey

--- pkg/autodiff/tamc_keys.h
+++ pkg/autodiff/tamc_keys.h
@@ -1,8 +1,8 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/autodiff/tamc_keys.h,v 1.1 2001/03/25 22:33:54 heimbach Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/autodiff/tamc_keys.h,v 1.1.6.1 2003/02/19 22:43:52 dimitri Exp $
 C
 C     /==========================================================\
 C     | tamc_keys.h keys required by TAMC for record computation |
 C     |==========================================================|
 C     \==========================================================/
-      integer           key, ikey, idkey
-      common /tamckeys/ key, ikey, idkey
+      integer           key, ikey
+      common /tamckeys/ key, ikey
