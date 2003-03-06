4e2f0d901c02ba3c66597a12dacf37a7851ba92d
Patrick Heimbach <heimbach@mit.edu>
2003-03-06 22:46:51 +0000

    merging c49 into e27

---
+++ pkg/aim/AIM_SIZE.h
@@ -0,0 +1,17 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/aim/Attic/AIM_SIZE.h,v 1.1.4.1 2003/03/06 22:46:50 heimbach Exp $
+C $Name:  $
+
+C     *==========================================================*
+C     | AIM_SIZE.h Declare size of arrays for AIM Physics        |
+C     *==========================================================*
+
+C     MITgcm declaration of grid size. 
+C     Latitudinal extent is one less than MITgcm ( i.e. NY-1)
+C     because MITgcm has dummy layer of land at northern most
+C     edge.  <-- no longer the case now
+#include "SIZE.h"
+
+C- dimension for AIM Physics package   
+      INTEGER NLON, NLAT, NLEV, NGP
+      PARAMETER ( NLON=sNx, NLAT=sNy, NLEV=Nr, NGP=NLON*NLAT ) 
+
