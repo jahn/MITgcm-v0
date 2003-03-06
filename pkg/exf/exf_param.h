fc938be6ef71758a01931b7522477c4deed5571d
Patrick Heimbach <heimbach@mit.edu>
2003-03-06 00:51:39 +0000

    o correcting small glitch in checkpoint_lev? o exf: synchronizing MAIN and ecco-branch o cal: synchronizing MAIN and ecco-branch

--- pkg/exf/exf_param.h
+++ pkg/exf/exf_param.h
@@ -1,4 +1,4 @@
-c $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/exf/Attic/exf_param.h,v 1.1.6.5 2003/03/04 00:17:40 heimbach Exp $
+c $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/exf/Attic/exf_param.h,v 1.1.6.6 2003/03/06 00:51:39 heimbach Exp $
 c
 c
 c     ==================================================================
@@ -31,6 +31,8 @@ c     HEADER exf_param
 c     ==================================================================
 
 c     Calendar data.
+      _RL     repeatPeriod
+
       integer hfluxstartdate1
       integer hfluxstartdate2
       integer hfluxstartdate(4)
@@ -198,6 +200,7 @@ c     File names.
       character*(128) apressurefile
 
       common /exf_param_i/
+     &                          repeatPeriod,
      &                          hfluxstartdate1,   hfluxstartdate2,
      &                          atempstartdate1,   atempstartdate2,
      &                          aqhstartdate1,     aqhstartdate2,
