68373a0a6186e18f9e541f29f8cc4c13b8433470
Alistair Adcroft <aadcroft@princeton.edu>
2003-05-13 18:18:05 +0000

    o split mon_set.F into mon_set_iounit.F and mon_set_pref.F  o replaced ref's to CPP_OPTIONS with MONITOR_OPTIONS  o added new s/r monitor_solution.F that checks that model state    and if unlikely lets the model die cleanly    - this is to reduce the number of hanging processes we encounter      if the model dies due to FPEs

---
+++ pkg/monitor/MONITOR_OPTIONS.h
@@ -0,0 +1,11 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/monitor/MONITOR_OPTIONS.h,v 1.1 2003/05/13 18:18:05 adcroft Exp $
+C $Name:  $
+
+C CPP options file for monitor package
+C
+C Use this file for selecting options within the monitor package
+
+#include "CPP_OPTIONS.h"
+
+C Disable use of hFacZ
+#undef MONITOR_TEST_HFACZ
