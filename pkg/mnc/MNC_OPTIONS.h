8ddab6ff88d35773676418daababeee92525abb0
Ed Hill <ed@eh3.com>
2004-01-05 06:20:08 +0000

    o initial check-in of notes and in-progress mnc package

---
+++ pkg/mnc/MNC_OPTIONS.h
@@ -0,0 +1,15 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/mnc/MNC_OPTIONS.h,v 1.1 2004/01/05 06:20:07 edhill Exp $
+C $Name:  $
+
+#ifndef MNC_OPTIONS_H
+#define MNC_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_MNC
+
+#include "CPP_OPTIONS.h"
+
+CEH3 package-specific options go here
+
+#endif /* ALLOW_MNC */
+#endif /* MNC_OPTIONS_H */
+
