719a354870a7f21da044a39aa1720a1162679de0
Dimitris Menemenlis <Dimitris.Menemenlis@jpl.nasa.gov>
2004-05-03 21:34:05 +0000

    recovering to checkpoint52m_post undoing some changes that were commited in error

--- verification/global_ocean.cs32x15/code/CPP_OPTIONS.h
+++ verification/global_ocean.cs32x15/code/CPP_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/global_ocean.cs32x15/code/CPP_OPTIONS.h,v 1.7 2004/04/06 01:24:28 jmc Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/global_ocean.cs32x15/code/CPP_OPTIONS.h,v 1.8 2004/05/03 21:34:04 dimitri Exp $
 C $Name:  $
 
 #ifndef CPP_OPTIONS_H
