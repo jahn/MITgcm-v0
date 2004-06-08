b4d24ce7d21bc19f9ee088b9a94a4ac108c1c44c
Andrea Molod <molod@ocean.mit.edu>
2004-06-08 16:12:53 +0000

    Assembling fizhi calls

--- pkg/fizhi/fizhi_land_SIZE.h
+++ pkg/fizhi/fizhi_land_SIZE.h
@@ -1,7 +1,7 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/fizhi/fizhi_land_SIZE.h,v 1.1 2004/06/07 18:11:37 molod Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/fizhi/fizhi_land_SIZE.h,v 1.2 2004/06/08 16:12:53 molod Exp $
 C $Name:  $
 
 c Land Grid Horizontal Dimension (Number of Tiles)
 c ------------------------------------------------
       integer nchp
-      parameter (nchp = 5)
+      parameter (nchp = sNx*sNy*10)
