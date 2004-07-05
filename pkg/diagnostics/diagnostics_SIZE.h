7686d85e041ab42a5c3f63eb5fb63e0e0b79116b
Ed Hill <ed@eh3.com>
2004-07-05 04:44:51 +0000

    o first steps towards putting diagnostics output into MNC/NetCDF

--- pkg/diagnostics/diagnostics_SIZE.h
+++ pkg/diagnostics/diagnostics_SIZE.h
@@ -1,7 +1,12 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/diagnostics/Attic/diagnostics_SIZE.h,v 1.2 2004/05/05 00:39:21 edhill Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/diagnostics/Attic/diagnostics_SIZE.h,v 1.3 2004/07/05 04:44:51 edhill Exp $
 C $Name:  $
 
 c Diagnostics Array Dimension
 c ---------------------------
       integer numdiags
       parameter (numdiags = 10)
+
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
