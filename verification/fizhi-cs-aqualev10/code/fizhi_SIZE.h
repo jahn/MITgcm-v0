9aafe0b80f2c385e011081246fc4c473cbefc781
Andrea Molod <molod@ocean.mit.edu>
2005-05-12 18:56:30 +0000

    Check in for verification experiment using fizhi in the control neale-hoskins aquaplanet simulation configuration

---
+++ verification/fizhi-cs-aqualev10/code/fizhi_SIZE.h
@@ -0,0 +1,7 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/fizhi-cs-aqualev10/code/Attic/fizhi_SIZE.h,v 1.1 2005/05/12 18:56:18 molod Exp $
+C $Name:  $
+
+c Physics Grid Vertical Dimension
+c -------------------------------
+      integer Nrphys
+      parameter (Nrphys = 20)
