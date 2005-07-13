d158c5fe8de8c62e9a03d97cac9ed835e38f56c8
Andrea Molod <molod@ocean.mit.edu>
2005-07-13 15:17:10 +0000

    New full fizhi verification experiment - initial check in

---
+++ verification/fizhi-cs-32x32x10/code/fizhi_SIZE.h
@@ -0,0 +1,7 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/fizhi-cs-32x32x10/code/Attic/fizhi_SIZE.h,v 1.1 2005/07/13 15:16:59 molod Exp $
+C $Name:  $
+
+c Physics Grid Vertical Dimension
+c -------------------------------
+      integer Nrphys
+      parameter (Nrphys = 20)
