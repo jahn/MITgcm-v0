8e6899150738694de5276914ca2fdfc4da9c7893
Andrea Molod <molod@ocean.mit.edu>
2005-12-01 19:01:06 +0000

    Changed surface pressure - need to change alternate grid levels

--- verification/fizhi-cs-aqualev10/code/fizhi_SIZE.h
+++ verification/fizhi-cs-aqualev10/code/fizhi_SIZE.h
@@ -1,7 +1,7 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/fizhi-cs-aqualev10/code/Attic/fizhi_SIZE.h,v 1.1 2005/05/12 18:56:18 molod Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/fizhi-cs-aqualev10/code/Attic/fizhi_SIZE.h,v 1.2 2005/12/01 19:01:06 molod Exp $
 C $Name:  $
 
 c Physics Grid Vertical Dimension
 c -------------------------------
       integer Nrphys
-      parameter (Nrphys = 20)
+      parameter (Nrphys = 21)
