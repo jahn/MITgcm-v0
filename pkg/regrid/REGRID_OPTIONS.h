a50cdb69fdd54d9286443942da97762ccf2e574f
Ed Hill <ed@eh3.com>
2006-08-15 04:05:48 +0000

    initial check-in

---
+++ pkg/regrid/REGRID_OPTIONS.h
@@ -0,0 +1,24 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/regrid/REGRID_OPTIONS.h,v 1.1 2006/08/15 04:05:48 edhill Exp $
+C $Name:  $
+
+C     Use this file to select options within the REGRID package
+
+#ifndef REGRID_OPTIONS_H
+#define REGRID_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_REGRID
+
+#include "CPP_OPTIONS.h"
+
+C     Place CPP define/undef flags here
+
+C     Currently, there are no compile-time options for the REGRID
+C     package.
+
+
+#endif /* ALLOW_REGRID */
+#endif /* REGRID_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
