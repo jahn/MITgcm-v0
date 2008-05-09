367cd1fe7d1d0bc10d6f82bf97000d870b509d67
David Ferreira <dfer@mit.edu>
2008-05-09 18:42:27 +0000

    A bit of cleanning.

--- verification/hs94.1x64x5/code_ad/COST_CPPOPTIONS.h
+++
@@ -1,8 +0,0 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/verification/hs94.1x64x5/code_ad/Attic/COST_CPPOPTIONS.h,v 1.2 2007/10/09 02:36:42 jmc Exp $
-C $Name:  $
-
-#include "CPP_OPTIONS.h"
-C
-C CPP flags controlling which code is included in the files that
-C will be compiled.
-C
