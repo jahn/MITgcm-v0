2297391abce798b573f2e35045b2235e03d208ec
Gael Forget <gforget@mit.edu>
2010-02-15 23:46:04 +0000

    smooth package -- going to the main repository

---
+++ pkg/smooth/SMOOTH_OPTIONS.h
@@ -0,0 +1,20 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/smooth/SMOOTH_OPTIONS.h,v 1.1 2010/02/15 23:46:04 gforget Exp $
+C $Name:  $
+
+C CPP options file for SMOOTH package
+C
+C Use this file for selecting options within the SMOOTH package
+
+#ifndef SMOOTH_OPTIONS_H
+#define SMOOTH_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_SMOOTH
+
+#include "CPP_OPTIONS.h"
+
+#endif /* ALLOW_SMOOTH */
+#endif /* SMOOTH_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
