3f03dc92c75de6084ed7abe984b861c0c75175c4
Gael Forget <gforget@mit.edu>
2010-08-06 18:40:01 +0000

    Prerequisite for adjoint of ggl90.

---
+++ pkg/ggl90/ggl90_ad_check_lev1_dir.h
@@ -0,0 +1,8 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/ggl90/ggl90_ad_check_lev1_dir.h,v 1.1 2010/08/06 18:38:50 gforget Exp $
+C $Name:  $
+
+CADJ STORE GGL90viscArU       = comlev1, key=ikey_dynamics
+CADJ STORE GGL90viscArV       = comlev1, key=ikey_dynamics
+CADJ STORE GGL90diffKr        = comlev1, key=ikey_dynamics
+CADJ STORE GGL90TKE           = comlev1, key=ikey_dynamics
+
