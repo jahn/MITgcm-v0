8758b1aa8f682af7ba4ba2c5c356ce7baac57655
Patrick Heimbach <heimbach@mit.edu>
2011-03-24 21:39:55 +0000

    Fix tapelev names (revert back to default).

--- pkg/ptracers/ptracers_ad_check_lev4_dir.h
+++ pkg/ptracers/ptracers_ad_check_lev4_dir.h
@@ -1,14 +1,14 @@
-C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/ptracers/ptracers_ad_check_lev4_dir.h,v 1.3 2011/03/24 12:17:39 heimbach Exp $
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/ptracers/ptracers_ad_check_lev4_dir.h,v 1.4 2011/03/24 21:39:55 heimbach Exp $
 C $Name:  $
 
 #ifdef ALLOW_PTRACERS
 # ifdef AUTODIFF_PTRACERS_SPLIT_FILES
 CADJ STORE ptracer(:,:,:,:,:,1:PTRACERS_num)
-CADJ &     = tapelev4_ptr, key = ilev_4
+CADJ &     = tapelev4, key = ilev_4
 CADJ STORE gptrnm1(:,:,:,:,:,1:PTRACERS_num)
-CADJ &     = tapelev4_ptr, key = ilev_4
+CADJ &     = tapelev4, key = ilev_4
 CADJ STORE gptr(:,:,:,:,:,1:PTRACERS_num)
-CADJ &     = tapelev4_ptr, key = ilev_4
+CADJ &     = tapelev4, key = ilev_4
 # else
 CADJ STORE ptracer = tapelev4, key = ilev_4
 CADJ STORE gptrnm1 = tapelev4, key = ilev_4
