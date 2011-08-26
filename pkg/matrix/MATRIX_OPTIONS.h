bbb7d80a454eb4253cc010b109de1d08bed9e2bb
Jean-Michel Campin <jmc@ocean.mit.edu>
2011-08-26 19:47:00 +0000

    - fix for multi-tiles (add bi,bj indices to exp/impMatrixCounter); - fix for multi-threaded (remove DATA statement); - use standard DIFFERENT_MULTIPLE function; - add MATRIX_OPTIONS.h

---
+++ pkg/matrix/MATRIX_OPTIONS.h
@@ -0,0 +1,14 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/matrix/MATRIX_OPTIONS.h,v 1.1 2011/08/26 19:47:00 jmc Exp $
+C $Name:  $
+
+#ifndef MATRIX_OPTIONS_H
+#define MATRIX_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_MATRIX
+
+C- Matrix pkg Macros and Options go here
+
+#endif /* ALLOW_MATRIX */
+#endif /* MATRIX_OPTIONS_H */
