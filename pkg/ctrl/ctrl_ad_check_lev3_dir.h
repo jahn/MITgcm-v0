7eedffe187b6b8ba461e2c719cec682b1d885b4d
Jean-Michel Campin <jmc@ocean.mit.edu>
2014-07-08 18:58:41 +0000

    add CVS Header & Name

--- pkg/ctrl/ctrl_ad_check_lev3_dir.h
+++ pkg/ctrl/ctrl_ad_check_lev3_dir.h
@@ -1,7 +1,8 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/ctrl/ctrl_ad_check_lev3_dir.h,v 1.3 2014/07/08 18:58:41 jmc Exp $
+C $Name:  $
 
 #ifdef ALLOW_GENTIM2D_CONTROL
 CADJ STORE xx_gentim2d  = tapelev3, key = ilev_3
 CADJ STORE xx_gentim2d0 = tapelev3, key = ilev_3
 CADJ STORE xx_gentim2d1 = tapelev3, key = ilev_3
 #endif
-
