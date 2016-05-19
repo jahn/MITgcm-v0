21a2af195813b191ce124e0a57f980f0c7f714b7
Matthew Mazloff <mmazloff@ucsd.edu>
2016-05-19 20:29:27 +0000

    adding  pkg/bling:   - intermediate complexity biogeochemical model   - see bling_description.txt for details   - verification experiment is global_oce_biogeo_bling

---
+++ pkg/bling/BLING_COST.h
@@ -0,0 +1,14 @@
+C $Header: /home/jahn/src/cvs2git/MITgcm/20170915-2/gcmpack-all-patch/MITgcm/pkg/bling/Attic/BLING_COST.h,v 1.1 2016/05/19 20:29:26 mmazloff Exp $
+C $Name:  $
+
+c BLING_COST.h
+         _RL totcost
+c control variables
+c add control variables here
+
+         COMMON /BLING_COST_CTRL/
+     &    totcost
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
